f02d030f5da33310b787e1d9108cc327b34cf6b5
Drozdnik <55313671+Drozdnik@users.noreply.github.com>
2020-05-16 10:44:24 +0300

    Update Source.cpp
    Полностью обновили Дерево Хаффмана. Нужно: создавать новый закодированный файл,  перекодировать его обратно, катологи.

--- prj/Project1/Project1/Source.cpp
+++ prj/Project1/Project1/Source.cpp
@@ -1,146 +1,725 @@
 #include <iostream>
-#include <vector>
-#include <map>
-#include <list>
-#include <fstream>
+
 using namespace std;
 
-class Node
+class slovo
+
 {
+
+protected:
+
+	char p_c[100]; // массив слова
+
+	int sm_num[40]; // считает колличество элементов
+
+	char p_list[40];//соответствие букв и колличества букв
+
+	int len;
+
 public:
-    int a;
-    char c;
-    Node* left, * right;
 
-    Node() { left = right = NULL; }
+	slovo(char* x) // инициализация
 
-    Node(Node* L, Node* R)
-    {
-        left = L;
-        right = R;
-        a = L->a + R->a;
-    }
+	{
+
+		int i = 0;
+
+		while (x[i] != '\0')
+
+		{
+
+			p_c[i] = x[i];
+
+			i++;
+
+		}
+
+		p_c[i] = '\0';
+
+		i = 0;
+
+		int j = 0;
+
+		int k = 0;
+
+		int f = 0;
+
+		int y3 = 0;
+
+		while (p_c[i] != '\0')
+
+		{
+
+			while (p_c[j] != '\0')
+
+			{
+
+				if ((p_c[i] == p_c[j]) and (j < i)) {
+
+					break;
+
+				}
+
+				p_list[y3] = p_c[i];
+
+				if (p_c[i] == p_c[j])
+
+				{
+
+					k++;
+
+				}
+
+				j++;
+
+			}
+
+			if (k != 0) {
+
+				sm_num[f] = k;
+
+				k = 0;
+
+				f++;
+
+				y3++;
+
+			}
+
+			i++;
+
+			j = 0;
+
+		}
+
+		p_list[y3] = '\0';
+
+		{//сортировка чисел
+
+			int j1 = 0;
+
+			int i1 = 1;
+
+			while (sm_num[j1] >= 0)
+
+			{
+
+				if (sm_num[j1] < 0)break;
+
+				while (sm_num[i1] >= 0)
+
+				{
+
+					if (sm_num[i1 - 1] > sm_num[i1])
+
+					{
+
+						int vs;
+
+						int vs1;
+
+						vs = sm_num[i1];
+
+						vs1 = p_list[i1];
+
+						sm_num[i1] = sm_num[i1 - 1];
+
+						p_list[i1] = p_list[i1 - 1];
+
+						sm_num[i1 - 1] = vs;
+
+						p_list[i1 - 1] = vs1;
+
+					}
+
+					i1++;
+
+				}
+
+				i1 = 1;
+
+				j1++;
+
+			}
+
+		}
+
+	}
+
+	void show_sm_num(void)
+
+	{
+
+		int i = 0;
+
+		while (sm_num[i] >= -1)
+
+		{
+
+			cout << sm_num[i] << " ";
+
+			i++;
+
+		}
+
+		cout << endl;
+
+	}//вывод
+
 };
 
+class bintree : public slovo
 
-struct MyCompare
 {
-    bool operator()(const Node* l, const Node* r) const { return l->a < r->a; }
+
+private:
+
+	int p[40][40];//тут хранятся коды всех символов
+
+	int pos_2[40][40]; //сумма ; как выставлять 0 и 1; последовательность элементов для присваивания 0 и 1
+
+	int pos_1[40];// позиции элементов в последовательности
+
+	int X[40];//запасной массив
+
+	int shifr[1000];//массив с шифром
+
+public:
+
+	bintree(char* x) :slovo(x)
+
+	{
+
+		int i = 0;// по основному массиву
+
+		int u = sm_num[0];//два элемента
+
+		int b = 0;//для суммы
+
+		int h = 0;// по элементам запасного массива
+
+		int g = 0;
+
+		int q;
+
+		int pos_i = 0;//по элементам pos_2
+
+		while (sm_num[g] > 0)
+
+		{
+
+			pos_1[g] = g;
+
+			g++;
+
+		}
+
+		cout << endl;
+
+		g = 0;
+
+		while (sm_num[g] > 0)
+
+		{
+
+			X[g] = sm_num[g];
+
+			g++;
+
+		}
+
+		//вывод
+
+		int i3 = 0;
+
+		while (X[i3] >= 0)
+
+		{
+
+			cout << X[i3] << " ";
+
+			i3++;
+
+		}
+
+		int i4 = 0;
+
+		cout << endl;
+
+		cout << " pos : ";
+
+		while (pos_1[i4] >= 0)
+
+		{
+
+			cout << pos_1[i4] << " ";
+
+			i4++;
+
+		}
+
+		cout << endl;
+
+		while (1)
+
+		{
+
+			if (X[1] < 0)break;
+
+			if (pos_1[0] == -1 and pos_1[1] == -1)
+
+			{
+
+				int i5 = 0;//чтобы найти сумму
+
+				int j5 = 1;
+
+				while (pos_2[i5][j5] >= 0)
+
+				{
+
+					if (pos_2[i5][0] == X[0]) {
+
+						while (pos_2[i5][j5] >= 0) {
+
+							pos_2[pos_i][1 + j5] = pos_2[i5][1 + j5];
+
+							j5++;
+
+						}
+
+						pos_2[i5][0] = 0;
+
+						pos_2[pos_i][1] = j5 - 2;
+
+					}
+
+					i5++;
+
+				}
+
+				int i6 = 0;
+
+				int j6 = 0;
+
+				while (pos_2[i6][j6] >= 0)
+
+				{
+
+					if (pos_2[i6][0] == X[1]) {
+
+						while (pos_2[i6][j6] >= 0) {
+
+							pos_2[pos_i][j5 + j6] = pos_2[i6][2 + j6];
+
+							j6++;
+
+						}
+
+						pos_2[i6][0] = 0;
+
+					}
+
+					i6++;
+
+				}
+
+			}
+
+			else if (pos_1[0] != -1 and pos_1[1] != -1)
+
+			{
+
+				pos_2[pos_i][1] = 1;
+
+				pos_2[pos_i][2] = pos_1[0];
+
+				pos_2[pos_i][3] = pos_1[1];
+
+			}
+
+			else if (pos_1[0] == -1)
+
+			{
+
+				int i5 = 0;//чтобы найти сумму
+
+				int j5 = 0;
+
+				while (pos_2[i5][j5] >= 0)
+
+				{
+
+					if (pos_2[i5][0] == X[0]) {
+
+						while (pos_2[i5][j5] >= 0) {
+
+							pos_2[pos_i][1 + j5] = pos_2[i5][1 + j5];
+
+							j5++;
+
+						}
+
+					}
+
+					i5++;
+
+				}
+
+				pos_2[pos_i][1] = j5 - 2;
+
+				pos_2[pos_i][j5] = pos_1[1];
+
+			}
+
+			else if (pos_1[1] == -1)
+
+			{
+
+				int i5 = 0;//чтобы найти сумму
+
+				int j5 = 0;
+
+				while (pos_2[i5][j5] >= 0)
+
+				{
+
+					if (pos_2[i5][0] == X[1]) {
+
+						while (pos_2[i5][j5] >= 0) {
+
+							pos_2[pos_i][2 + j5] = pos_2[i5][1 + j5];
+
+							j5++;
+
+						}
+
+						pos_2[i5][0] = 0;
+
+					}
+
+					i5++;
+
+				}
+
+				pos_2[pos_i][1] = 1;
+
+				pos_2[pos_i][2] = pos_1[0];
+
+			}
+
+			X[0] = X[0] + X[1];
+
+			pos_1[0] = -1;
+
+			pos_2[pos_i][0] = X[0];
+
+			pos_i++;
+
+			int i2 = 1;
+
+			while (X[i2] > 0)
+
+			{
+
+				X[i2] = X[i2 + 1];
+
+				pos_1[i2] = pos_1[i2 + 1];
+
+				i2++;
+
+			}
+
+			i++;
+
+			//сортировка по позрастанию
+
+			int j1 = 0;
+
+			int i1 = 1;
+
+			while (X[j1] >= 0)
+
+			{
+
+				if (X[j1] < 0)break;
+
+				while (X[i1] >= 0)
+
+				{
+
+					if (X[i1 - 1] > X[i1])
+
+					{
+
+						int vs1;
+
+						int vs;
+
+						vs1 = pos_1[i1];
+
+						vs = X[i1];
+
+						pos_1[i1] = pos_1[i1 - 1];
+
+						X[i1] = X[i1 - 1];
+
+						pos_1[i1 - 1] = vs1;
+
+						X[i1 - 1] = vs;
+
+					}
+
+					i1++;
+
+				}
+
+				i1 = 1;
+
+				j1++;
+
+			}
+
+			//вывод
+
+			int i3 = 0;
+
+			while (X[i3] >= 0)
+
+			{
+
+				cout << X[i3] << " ";
+
+				i3++;
+
+			}
+
+			cout << endl;
+
+			i3 = 0;
+
+			cout << " pos : ";
+
+			while (X[i3] >= 0)
+
+			{
+
+				cout << pos_1[i3] << " ";
+
+				i3++;
+
+			}
+
+			cout << endl;
+
+		}
+
+		//выставление 0 и 1
+
+		i = 0;
+
+		int j = 0;
+
+		int summ = 0;
+
+		int pos_k = 1;
+
+		while (pos_2[summ][0] >= -1) {
+
+			summ++;
+
+		}
+
+		while (summ >= 0)
+
+		{
+
+			while (pos_k <= pos_2[summ][1])
+
+			{
+
+				int y = 0;
+
+				while (1) {
+
+					if (p[pos_2[summ][1 + pos_k]][y] < 0)break;
+
+					y++;
+
+				}
+
+				p[pos_2[summ][1 + pos_k]][y] = 0;
+
+				pos_k++;
+
+			}
+
+			while (pos_2[summ][pos_k] >= 0)
+
+			{
+
+				int y = 0;
+
+				while (1) {
+
+					if (p[pos_2[summ][1 + pos_k]][y] < 0)break;
+
+					y++;
+
+				}
+
+				p[pos_2[summ][1 + pos_k]][y] = 1;
+
+				pos_k++;
+
+			}
+
+			pos_k = 1;
+
+			summ--;
+
+		}
+
+	};
+
+	void show(void)
+
+	{
+
+		int i = 0;
+
+		int j = 0;
+
+		while (pos_2[i][j] >= 0)
+
+		{
+
+			while (pos_2[i][j] >= -1) {
+
+				cout << pos_2[i][j] << " ";
+
+				j++;
+
+			}
+
+			j = 0;
+
+			cout << endl;
+
+			i++;
+
+		}
+
+		cout << endl;
+
+		i = 0;
+
+		j = 0;
+
+		while (p[i][j] >= -1)
+
+		{
+
+			cout << p_list[i] << " = ";
+
+			while (p[i][j] >= -1) {
+
+				cout << p[i][j] << " ";
+
+				j++;
+
+			}
+
+			j = 0;
+
+			cout << endl;
+
+			i++;
+
+		}
+
+	}
+
+	void shifr_m(void)
+
+	{
+
+		int i = 0;
+
+		int j = 0;
+
+		int f = 0;
+
+		while (p_c[i] != '\0')
+
+		{
+
+			if (p_c[i] == p_list[j])
+
+			{
+
+				int k = 0;
+
+				while (p[j][k] >= -1)
+
+				{
+
+					shifr[f] = p[j][k];
+
+					k++;
+
+					f++;
+
+				}
+
+				i++;
+
+				j = -1;
+
+			}
+
+			j++;
+
+		}
+
+	}
+
+	void shifr_v(void) {
+
+		int i = 0;
+
+		cout << "shifr := ";
+
+		while (shifr[i] >= -1) {
+
+			cout << shifr[i];
+
+			i++;
+
+		}
+
+		cout << endl;
+
+	}
+
 };
 
+char st[100];
 
-vector<bool> code;
-map<char, vector<bool> > table;
+int main()
 
-void BuildTable(Node* root)
 {
-    if (root->left != NULL)
-    {
-        code.push_back(0);
-        BuildTable(root->left);
-    }
 
-    if (root->right != NULL)
-    {
-        code.push_back(1);
-        BuildTable(root->right);
-    }
+	cin.get(st, 20);
 
-    if (root->left == NULL && root->right == NULL) table[root->c] = code;
-    code.pop_back();
+	bintree p_1(st);
+
+	p_1.show();
+
+	p_1.shifr_m();
+
+	p_1.shifr_v();
+
+	return 0;
+
 }
-
-
-int main(int argc, char* argv[])
-{
-    
-    ifstream f("1.txt", ios::out | ios::binary);
-
-    map<char, int> m;
-
-    while (!f.eof())
-    {
-        char c = f.get();
-        m[c]++;
-    }
-
-
-  
-
-    list<Node*> t;
-    for (map<char, int>::iterator itr = m.begin(); itr != m.end(); ++itr)
-    {
-        Node* p = new Node;
-        p->c = itr->first;
-        p->a = itr->second;
-        t.push_back(p);
-    }
-
-
- 
-
-    while (t.size() != 1)
-    {
-        t.sort(MyCompare());
-
-        Node* SonL = t.front();
-        t.pop_front();
-        Node* SonR = t.front();
-        t.pop_front();
-
-        Node* parent = new Node(SonL, SonR);
-        t.push_back(parent);
-
-    }
-
-    Node* root = t.front();   
-
-
-
-    BuildTable(root);
-
-  
-
-    f.clear(); f.seekg(0); 
-
-    ofstream g("output.txt", ios::out | ios::binary);
-
-    int count = 0; char buf = 0;
-    while (!f.eof())
-    {
-        char c = f.get();
-        vector<bool> x = table[c];
-        for (int n = 0; n < x.size(); n++)
-        {
-            buf = buf | x[n] << (7 - count);
-            count++;
-            if (count == 8) { count = 0;   g << buf; buf = 0; }
-        }
-    }
-
-    f.close();
-    g.close();
-
-   
-    ifstream F("output.txt", ios::in | ios::binary);
-
-    setlocale(LC_ALL, "Russian"); 
-
-    Node* p = root;
-    count = 0; char byte;
-    byte = F.get();
-    while (!F.eof())
-    {
-        bool b = byte & (1 << (7 - count));
-        if (b) p = p->right; else p = p->left;
-        if (p->left == NULL && p->right == NULL) { cout << p->c; p = root; }
-        count++;
-        if (count == 8) { count = 0; byte = F.get(); }
-    }
-
-    F.close();
-
-    return 0;
-}
-
-
